c85b1edb27089db4e033001e4be1e09bfca03931
Amit Tiwari <70047168+Amit-Tiwari1@users.noreply.github.com>
2024-01-20 17:03:28 +0530

    adding expenses

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,7 +17,7 @@ const registerUser = asyncHandler(async (req, res) => {
   // });
 
   const { fullName, email, username, password } = req.body;
-  console.log("email", email);
+
   if ([fullName, email, username, password].some(() => field?.trim() === "")) {
     throw new ApiError(400, "All fields are required!");
   }
